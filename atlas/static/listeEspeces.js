c55bb492036119460cd014a2e88d6e04edd6ee38
Jérôme Maruéjouls <jerome.maruejouls.pro@gmail.com>
2025-06-23 08:39:24 +0000

    ajout des filtres de taxons sur les fiches especes

--- atlas/static/listeEspeces.js
+++ atlas/static/listeEspeces.js
@@ -13,3 +13,68 @@ $(document).ready(function(){
     });
   });
 });
+
+function filterOiseaux(e){
+  document.getElementById('taxonInput').value = "Oiseaux";
+    $("#taxonList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf("oiseaux") > -1)
+    });
+  e.preventDefault();
+}
+
+function filterMammifères(e){
+  document.getElementById('taxonInput').value = "Mammifères";
+    $("#taxonList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf("mammiferes") > -1)
+    });
+  e.preventDefault();
+}
+
+function filterInsectes(e){
+  document.getElementById('taxonInput').value = "Insectes";
+    $("#taxonList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf("insectes") > -1)
+    });
+  e.preventDefault();
+}
+
+function filterAll(e){
+  document.getElementById('taxonInput').value = "";
+    $("#taxonList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf("") > -1)
+    });
+  e.preventDefault();
+}
+
+function filterReptiles(e){
+  document.getElementById('taxonInput').value = "Reptiles";
+    $("#taxonList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf("reptiles") > -1)
+    });
+  e.preventDefault();
+}
+
+//Filtres des plantes :
+function filterAngiospermes(e){
+  document.getElementById('taxonInput').value = "Angiospermes";
+    $("#taxonList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf("angiospermes") > -1)
+    });
+  e.preventDefault();
+}
+
+function filterGymnospermes(e){
+  document.getElementById('taxonInput').value = "Gymnospermes";
+    $("#taxonList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf("gymnospermes") > -1)
+    });
+  e.preventDefault();
+}
+
+function filterFougeres(e){
+  document.getElementById('taxonInput').value = "Fougères";
+    $("#taxonList li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf("fougeres") > -1)
+    });
+  e.preventDefault();
+}
